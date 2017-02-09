a459b8240ffe66eb54f409524f9098645448fcf1
MHSAJ <tassiwomack@mwc-066012.dhcp.missouri.edu>
2017-02-09 14:33:41 -0600

    Cleaned up path stuff and ran linear model

--- NGSL.R
+++ NGSL.R
@@ -2,6 +2,10 @@
 library("rio")
 ngsl.data <- import("/Users/tassiwomack/Desktop/MasterRespones#1.xlsx")
 
+ngsl.data<-import("/Users/ericwulff/Desktop/MHS/NGSLallresponses.csv")
+
+
+
 #need to delete the 2nd column cause its junk [2] refers to second column, NULL = gone
 ngsl.data[2] <- NULL
 ngsl.data[17] <- NULL
@@ -63,8 +67,8 @@ ngsl.data$cr3<-as.numeric(as.character(ngsl.data$cr3))
 ngsl.data$cl3<-as.numeric(as.character(ngsl.data$cl3))
 
 ## averaging the column scores per response and creating new columns 
-finaldata<-mutate(ngsl.data, avg1=((a1+e1+cr1+cl1)/4), avg2=((a2+e2+cr2+cl2)/4),avg3=((a3+e3+cr3+cl3)/4))
-finaldata
+ngsl.data<-mutate(ngsl.data, avg1=((a1+e1+cr1+cl1)/4), avg2=((a2+e2+cr2+cl2)/4),avg3=((a3+e3+cr3+cl3)/4))
+ngsl.data
 ## run t-test on averages 
 t.test(finaldata$avg1,finaldata$avg3) ###ttest with two numeric variables###
 t.test(finaldata$avg2,finaldata$avg3)
@@ -75,6 +79,9 @@ write.csv(finaldata, file="NGSLallresponses.csv")
 ####################################
 
 ngsl.data<-import("/Users/ericwulff/Desktop/MHS/NGSLallresponses.csv")
+
+ngsl.data <- import('/Users/tassiwomack/NGSLallresponses.csv')
+
 ngsl.data<-mutate(ngsl.data, davg13= (avg3-avg1)) ##add column with difference between avg3 and avg1 called davg13##
 ngsl.data<-mutate(ngsl.data, avgtotal= ((avg1+avg2+avg3)/2))##add average total #33
 
@@ -82,6 +89,10 @@ cor(ngsl.data$grade,ngsl.data$davg13) ## correlation between grade and differenc
 cor(ngsl.data$grade,ngsl.data$avg3)
 cor(ngsl.data$grade,ngsl.data$avgtotal)
 cor(ngsl.data$grade,ngsl.data$avgtotal)
+
+### run linear model 
+mod1 <- lm(grade ~ avgtotal, data = ngsl.data)
+summary(mod1)
 
 
 
